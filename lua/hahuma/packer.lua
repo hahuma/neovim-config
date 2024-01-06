e176017670c74c6b72b568abed7e6b34b8c70c3b
Hahuma <firminopetterson@gmail.com>
2024-01-06 07:25:02 -0300

    add lspzero and vim-fugitive

--- lua/hahuma/packer.lua
+++ lua/hahuma/packer.lua
@@ -30,4 +30,26 @@ return require('packer').startup(function(use)
   use 'nvim-treesitter/playground'
 
   use 'theprimeagen/harpoon'
+
+  use 'mbbill/undotree'
+
+  use 'tpope/vim-fugitive'
+
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v3.x',
+	  requires = {
+	    -- LSP Support
+	    {'neovim/nvim-lspconfig'},
+	    -- Autocompletion
+	    {'hrsh7th/nvim-cmp'},
+	    {'hrsh7th/cmp-nvim-lsp'},
+	    {'L3MON4D3/LuaSnip'},
+	  }
+	}
+  use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  }
 end)
